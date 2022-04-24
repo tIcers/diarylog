5a7d15189846beaeda01f1a88dabaa8f787ff6f7
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-04-24 01:51:57 -0700

    Created models, setup user and topics

---
+++ learning_logs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Topic(models.Model):
+    """Topics the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
