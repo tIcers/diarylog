bf5bad4e3a557d80b7f211d13f86af4cb3ec2986
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-04-27 22:38:10 -0700

    Added Meta class with few entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,5 +14,14 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Something specific learned about a topic"""
-    topic = models.Foreingkey(Topic,on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representaino of the model."""
+        return f"{self.text[:50]}"
