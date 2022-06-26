a863dddf7af6a742967122e448177e12e7e83658
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-26 21:41:39 +0900

    let user enter their entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
 	path('topics/',views.topics, name='topics'),
 
 	#Detail page for single topic
-	path('topics/<int:topic_id>',views.topic, name='topic')
+	path('topics/<int:topic_id>',views.topic, name='topic'),
+
+	# Page for adding a new topic
+	path('new_topic/', views.new_topic, name='new_topic')
 	
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 from .models import Topic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -20,3 +24,21 @@ def topic(request,topic_id):
 	entries = topic.entry_set.order_by('-date_added') # revrse order
 	context = {'topic':topic,'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+	"""Add a new topic"""
+	if request.method !="POST":
+		#no data submitted; create a blank form 
+		form = TopicForm()
+	else:
+		#POst data submitted; process data
+		form = TopicForm(request.POST)
+		
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+	
+	context = {'form':form}
+	
+
+	return render(request,'learning_logs/new_topic.html',context)
